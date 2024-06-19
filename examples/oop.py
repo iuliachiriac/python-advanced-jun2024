111fe8998e316da897367fb66280fc26cb1355cc
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-06-19 15:30:57 +0300

    Add static/class methods example

--- examples/oop.py
+++ examples/oop.py
@@ -2,9 +2,12 @@ from datetime import date
 
 
 class Person:
+    count = 0  # class attribute
+
     def __init__(self, name, date_of_birth):
-        self.name = name
+        self.name = name  # instance attribute
         self.date_of_birth = date_of_birth
+        self.increment_count()
 
     def __int__(self):
         return self.date_of_birth.year
@@ -21,6 +24,26 @@ class Person:
 
     def __le__(self, other):
         return self.date_of_birth >= other.date_of_birth
+
+    def get_age(self):
+        return self.years_passed_since(self.date_of_birth)
+
+    @classmethod
+    def increment_count(cls):
+        cls.count += 1
+
+    @staticmethod
+    def years_passed_since(date_obj):
+        today = date.today()
+        age = today.year - date_obj.year
+        if (date_obj.month, date_obj.day) > (today.month, today.day):
+            age -= 1
+        return age
+
+
+class Student(Person):
+    def __init__(self, name, date_of_birth):
+        super().__init__(name, date_of_birth)
 
 
 if __name__ == "__main__":
@@ -39,3 +62,14 @@ if __name__ == "__main__":
     print(p2 <= p3)  # p1.__le__(p2)
     print(p1 > p2)
     print(p2 == p3)
+
+    print(p1.get_age())
+    print(Person.get_age(p1))
+
+    print(p1.count is Person.count)
+
+    # p1.increment_count()
+    # Person.increment_count()
+    print("Person count:", Person.count)
+
+    print(Person.years_passed_since(date(1918, 12, 1)))
